7f131034ce2ab117eacf9872362833990abbfdd5
Diego Costa <diego@diegocosta.com.br>
2018-04-05 18:53:01 -0300

    Reorganizado o componente de Loading

--- src/components/App.js
+++ src/components/App.js
@@ -1,58 +1,64 @@
 import React, { Component } from 'react';
 import Header from './Header';
-import Filtros from './Filtros';
-import Listagem from './Listagem';
+import Filters from './Filters';
+import List from './List';
+import Loading from './Loading';
+
+let booksRaw = [];
 
 class App extends Component {
   state = {
-    produtos : [],
-    lista_produtos : []
+    books : null
   }
 
-  constructor(props) {
-    super(props);
-
-    fetch('http://demo6669321.mockable.io/livros')
-      .then(resposta => resposta.json().then(({ data }) => this.setState({ produtos: data, lista_produtos: data })))
-      .catch(error => console.log(error));
+  componentDidMount() {
+    fetch('http://demo6669321.mockable.io/livros').then(resposta => resposta.json().then(({ data }) => {
+      booksRaw = data;
+      this.setState({ books : booksRaw })
+    }));
   }
 
   filtrarPorCategoria = categoria => {
-    const { produtos } = this.state;
-    const porCategoria = (categoria) ? produtos.filter(item => item.categoria === categoria) : produtos;
+    const porCategoria = (categoria) ? booksRaw.filter(item => item.categoria === categoria) : booksRaw;
     
-    this.setState({ lista_produtos : porCategoria });
+    this.setState({ books : porCategoria });
   }
 
   ordenarPorNome = () => {
-    const { lista_produtos } = this.state;
-    const porNome = lista_produtos.sort((atual, proximo) => {
+    const { books } = this.state;
+    const porNome = books.sort((atual, proximo) => {
       if (atual.titulo > proximo.titulo) return 1;
       if (atual.titulo < proximo.titulo) return -1;
       return 0;
     });
 
-    this.setState({ lista_produtos: porNome });
+    this.setState({ books: porNome });
   }
 
   ordenarPorPreco = () => {
-    const { lista_produtos } = this.state;
-    const porPreco = lista_produtos.sort((atual, proximo) => atual.preco - proximo.preco);
+    const { books } = this.state;
+    const porPreco = books.sort((atual, proximo) => atual.preco - proximo.preco);
 
-    this.setState({ lista_produtos: porPreco });
+    this.setState({ books: porPreco });
   }
 
   render() {
+    const { books } = this.state;
+
+    if(books === null) {
+      return <Loading />
+    }
+
     return (
       <div className="App">
         <Header />
         <section className="content">
-          <Filtros
+          <Filters
             ordenarPorNome={this.ordenarPorNome}
             ordenarPorPreco={this.ordenarPorPreco}
             filtrarPorCategoria={this.filtrarPorCategoria}
           />
-          <Listagem lista={this.state.lista_produtos} />
+          <List list={this.state.books} />
         </section>
       </div>
     );
